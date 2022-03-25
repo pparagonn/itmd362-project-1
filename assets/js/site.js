a3bd9f6f16099fcb3b24ac6275122ebe3779e46c
Jovany Melchor <jmelchor@hawk.iit.edu>
2022-03-25 00:50:13 -0500

    Add utility functions

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,3 +2,54 @@
 
 var html = document.querySelector('html');
 html.classList.add('js');
+
+
+
+
+
+
+
+/**
+  * UTILITY FUNCTIONS
+  */
+
+// debounce to not execute until after an action has stopped (delay)
+function debounce(callback, delay) {
+  var timer; // function-scope timer to debounce()
+  return function() {
+    var context = this; // track function-calling context
+    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+    var args = arguments; // hold onto arguments object
+    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+
+    // Reset the timer
+    clearTimeout(timer);
+
+    // Set the new timer
+    timer = setTimeout(function() {
+      // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+      callback.apply(context, args);
+    }, delay);
+  }
+}
+
+// throttle to slow execution to a certain amount of elapsed time (limit)
+function throttle(callback, limit) {
+  var throttling; // function-scope boolean for testing throttle state
+  return function() {
+    var context = this; // track function-calling context
+    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+    var args = arguments; // hold onto arguments object
+    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+
+    // Run the function if not currently throttling
+    if (!throttling) {
+      // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+      callback.apply(context, args);
+      throttling = true;
+      setTimeout(function() {
+        throttling = false;
+      }, limit);
+    }
+  }
+}
